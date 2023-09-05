4adcb4356346737797dfbb60d9d527c4fd976ea8
MichaelSetyawan11 <129755316+MichaelSetyawan11@users.noreply.github.com>
2023-09-05 19:17:49 +0700

    Add files via upload

---
+++ deretan angka.java
@@ -0,0 +1,22 @@
+public class deretan angka ;
+    public static void main (String [] args){
+        for (int i=1; i<6; i+=1){
+            System.out.print(i + " ");
+    }
+        System.out.println("");
+            for (int n=2; n<11; n+=2){
+                System.out.print(n + " ");
+    }
+        System.out.println("");
+            for (int x=3; x<16; x+=3){
+                System.out.print(x + " ");
+    }
+        System.out.println("");
+            for (int y=4; n<21; y+=4){
+                System.out.print(y + " ");
+    }
+        System.out.println("");
+            for (int z=5; z<26; z+=5){
+                System.out.print(z + " ");
+    }
+}
